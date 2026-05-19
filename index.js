ef43d5df60e32710ed5316ebf787e506e5cdcf46
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-05-19 18:20:52 +0600

    Database connected and basic packages are added and server fully working

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine !!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
